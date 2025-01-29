4f9822f43df8a76d7ba2bb92af12f45167307130
hs2405 <143207734+hs2405@users.noreply.github.com>
2025-01-29 19:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,20 @@ import io
 
 app = Flask(__name__)
 
-# Function to execute the user code and capture output or error
 def execute_code(user_code):
     try:
-        # Redirect stdout to capture print statements from the code
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
-        # Execute the user-provided code
         exec(user_code)
 
-        # Restore the original stdout
         sys.stdout = sys.__stdout__
 
         return "Code executed successfully! Output:\n" + captured_output.getvalue()
 
     except Exception as e:
-        # If an exception occurs, capture the error message
-        sys.stdout = sys.__stdout__  # Restore the original stdout
+        
+      sys.stdout = sys.__stdout__  
         return f"Error in code:\n{str(e)}"
 
 @app.route("/", methods=["GET", "POST"])
